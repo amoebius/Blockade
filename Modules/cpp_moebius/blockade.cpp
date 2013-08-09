29e0f021e1f0dd747747c94fe8aef6d4f664506b
James Payor <j.payor@hotmail.com>
2013-08-10 05:46:26 +1000

    New blockade interface for C++ fully implemented (untested).

--- Modules/cpp_moebius/blockade.cpp
+++ Modules/cpp_moebius/blockade.cpp
@@ -22,10 +22,13 @@ static const string str_move("move"), str_block("block"), str_turn("turn"), str_
 // Global information:
 static int boardSize, myPID, player_x[2], player_y[2];
 
-// Move information:
-static const int nothing = 0, moving = 1, blocking = 2;
-static int playerMove, moveDir, moveX, moveY;
-static string errorMsg;
+// Explanatory methods for extracting information from the "action" bit-hacked datastructure:
+static const int moving = 0, blocking = 1, invalid = 2;
+static int getActionType(action act);
+static int getMoveDir(action act);
+static int getBlockX(action act);
+static int getBlockY(action act);
+
 
 int main() {
 	// Allow the client to set a name and colour:
@@ -79,17 +82,14 @@ int main() {
 
 		} else if (command == str_turn) {
 			// Make a move:
-			playerMove = nothing;
-			errorMsg = "No move was made by the client.";
-			clientDoTurn();
+			action turn = getMove();
+			int actionType = getActionType(turn);
 
-			if(playerMove == moving) {
-				cout << str_move << ' ' << moveDir << endl;
-
-			} else if(playerMove == blocking) {
-				cout << str_block << ' ' << moveX << ' ' << moveY << endl;
-
-			} else { // No move was made - indicate this.
+			if(actionType == moving) {
+				cout << str_move << ' ' << getMoveDir(turn) << endl;
+			} else if (actionType == blocking) {
+				cout << str_block << ' ' << getBlockX(turn) << ' ' << getBlockY(turn) << endl;
+			} else { // Invalid move:
 				cout << str_nothing << endl;
 			}
 
@@ -101,23 +101,61 @@ int main() {
 	return EXIT_SUCCESS;
 }
 
-// Registration functions:
-void setName(const char* name) {
-	player_name = name;
-}
-void setColour(int r, int g, int b) {
-	red = r;
-	green = g;
-	blue = b;
+
+// Converts RGB components into a single integer:
+color makeColor(int r, int g, int b) {
+	return (r<<16) + (g<<8) + b;
 }
 
-// Move-making functions:
-void movePosition(int dir) {
-	playerMove = moving;
-	moveDir = dir;
+// Converts a direction to an integer in (-1, -2, -3, -4):
+action step(int dir) {
+	return -1 - dir;
 }
-void blockSquare(int x, int y) {
-	playerMove = blocking;
-	moveX = x;
-	moveY = y;
+
+// Converts the coordinates into a postive integer:
+action block(int x, int y) {
+	return (x<<16) + y;
+}
+
+// Returns the type of action the number specifies:
+static int getActionType(action act) {
+	if(act < 0) return moving;
+	else return blocking;
+}
+
+// Extracts the movement direction, mapping (-1,-2,-3,-4) to (0,1,2,3):
+static int getMoveDir(action act) {
+	return -1 - act;
+}
+
+// Extracts the x coordinate - the higher 16 bits of the action:
+static int getBlockX(action act) {
+	return act>>16;
+}
+// Extracts the y coordinate - the lower 16 bits of the action:
+static int getBlockY(action act) {
+	return act & ((1<<16) - 1);
+}
+
+// Returns the player's ID:
+int getMyID() {
+	return myPID;
+}
+// Returns the opponent's ID:
+int getTheirID() {
+	return 1 - myPID;
+}
+
+// Returns the board size:
+int getBoardSize() {
+	return boardSize;
+}
+
+// Returns the specified player's x coordinate:
+int getX(int id) {
+	return player_x[id];
+}
+// Returns the specified player's y coordinate:
+int getY(int id) {
+	return player_y[id];
 }
