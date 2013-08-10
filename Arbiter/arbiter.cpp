c15808be3993d2671ef5f8d7eb08219afd373f79
James Payor <j.payor@hotmail.com>
2013-08-10 22:52:46 +1000

    Initial 'Arbiter' files in place, threading library committed.

---
+++ Arbiter/arbiter.cpp
@@ -0,0 +1,9 @@
+/* -------------------------------------------
+ * Moebius Blockade Interface for C++
+ * -------------------------------------------
+ * Implements the interface between the Arbiter and client as specified by the 'ArbiterProtocol' and
+ * 'blockade.h' files for the 'Blockade' game.
+ * -------------------------------------------
+ * Author:  James Payor (github.com/amoebius)
+ * -------------------------------------------
+ */
---
+++ Arbiter/arbiter.hpp
@@ -0,0 +1,9 @@
+/* -------------------------------------------
+ * Arbiter - arbiter.hpp
+ * -------------------------------------------
+ * Declares the methods used by the Arbiter.
+ * The Arbiter is responsible for running games
+ * -------------------------------------------
+ * Author:  James Payor (github.com/amoebius)
+ * -------------------------------------------
+ */
