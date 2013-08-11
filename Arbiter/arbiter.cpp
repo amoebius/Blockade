847810ef55fb7fb419386ebe51c2480d9476d1d9
James Payor <j.payor@hotmail.com>
2013-08-12 04:17:38 +1000

    Initial (useless) arbiter.cpp

--- Arbiter/arbiter.cpp
+++ Arbiter/arbiter.cpp
@@ -1,9 +1,38 @@
 /* -------------------------------------------
- * Moebius Blockade Interface for C++
+ * Arbiter - arbiter.cpp
  * -------------------------------------------
- * Implements the interface between the Arbiter and client as specified by the 'ArbiterProtocol' and
- * 'blockade.h' files for the 'Blockade' game.
+ * Implements the Arbiter for 'Blockade' - given the locations of two bots
+ * as commandline arguments, it will have the bots play against each other
+ * and log the game to standard output.
  * -------------------------------------------
  * Author:  James Payor (github.com/amoebius)
  * -------------------------------------------
  */
+
+#include <cstddef>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "arbiter.hpp"
+#include "iopipe.hpp"
+
+
+int main(int argc, char* argv[]) {
+
+	string programs[2];
+	if(argc == 1) {
+		programs[0] = programs[1] = argv[0];
+	} else if(argc == 2) {
+		for(int i=0; i<2; ++i) programs[i] = argv[i];
+	} else {
+		cerr << "Invalid number of arguments specified.  Usage:\n" << "arbiter first_bot_location [second_bot_location]\n";
+		return EXIT_FAILURE;
+	}
+
+	int pipes[2][2][2];
+
+
+
+	return EXIT_SUCCESS;
+}
