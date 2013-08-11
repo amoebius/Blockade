004eac406bb38493b093b6b9bd7e4a470438efff
James Payor <j.payor@hotmail.com>
2013-08-12 06:13:26 +1000

    'iopipe' library implemented and documented, yet to be tested!

---
+++ Arbiter/iopipe/iopipe.cpp
@@ -0,0 +1,128 @@
+/* -------------------------------------------
+ * iopipe - iopipe.cpp
+ * -------------------------------------------
+ * Implements the 'iopipe' class, which manages unix pipe io via a stream interface.
+ * -------------------------------------------
+ * Author:  James Payor (github.com/amoebius)
+ * -------------------------------------------
+ */
+
+/* NOTE:  (IMPORTANT)
+ * This module needs a hacky method to convert a pipe file descriptor into istreams
+ * ostreams, or replicate that functionality.  The author envisages two methods:
+ *  1) Use stringstreams to replicate high-level stream io, and write/read from the pipe
+ *     to fill/empty the stringstreams.
+ *  2) Use non-standard classes to build a stream around a POSIX file descriptor.
+ *
+ * THIS implementation uses OPTION 2.  There is a chance that it will not complie given
+ * that "__gnu_cxx::stdio_filebur<char>" is used.  If this is the case, the functionality
+ * can be replicated with stringstreams, but for now this has not been implemented, as
+ * wrapping the file descriptors directly simplifies matters considerably.
+ *
+ */
+
+#include <unistd.h>
+#include "iopipe.hpp"
+
+
+/***********************************************************************************************************/
+/*  "iopipe" Implementation:                                                                               */
+/***********************************************************************************************************/
+
+// Creates an iopipe attached to a newly instantiated pipe, setting open state to false and invoking open():
+iopipe::iopipe() : isOpen(false) {
+	open();
+}
+
+// Creates an iopipe that is not attached to a new pipe, in a closed state:
+iopipe::iopipe(int) : isOpen(false) {}
+
+// Binds the given file descriptors to the new iopipe by invoking the relevant constructors, and sets state to 'open':
+iopipe::iopipe(int in_fd, int out_fd) : in(in_fd), out(out_fd), isOpen(true) {}
+
+// Binds the given ipipe and opipe to the iopipe, useful for combining two separate streams into a unified interface:
+iopipe::iopipe(ipipe in, opipe out) : in(in), out(out) {}
+
+// Copy constructor - simply copy the other iopipe's member variables:
+iopipe::iopipe(const iopipe& other) : in(other.in), out(other.out), isOpen(other.isOpen) {}
+
+// Assignment - simply assign member variables to the other iopipe's members:
+iopipe& iopipe::operator = (const iopipe& other) {
+	in = other.in;
+	out = other.out;
+	isOpen = other.isOpen;
+	return *this;
+}
+
+// Returns an ipipe bound to the input file the pipe employs:
+const ipipe iopipe::get_in() {
+	return in;
+}
+
+// Returns an opipe bound to the output file the pipe employs:
+const opipe iopipe::get_out() {
+	return out;
+}
+
+// Creates a new pipe, binding its ends to the iopipe's ipipe and opipe components:
+void iopipe::open() {
+	const int READ = 0, WRITE = 1; // Constant indices into the 'pipe_fd' array.
+	int pipe_fd[2];
+	pipe(pipe_fd);                 // Create a new pipe.
+	in = ipipe(pipe_fd[READ]);     // Bind the 'read' end to the ipipe.
+	out = opipe(pipe_fd[WRITE]);   // Bind the 'write' end to the opipe.
+	isOpen = true;                 // Update open state.
+}
+
+// Binds the given file descriptors to this iopipe, invoking the relevant 'open' methods:
+void iopipe::open(int in_fd, int out_fd) {
+	in.open(in_fd);
+	out.open(out_fd);
+	isOpen = true;
+}
+
+// Closes the iopipe by invoking the relevant close methods and updating its state flag:
+void iopipe::close() {
+	if(isOpen) {
+		in.close();
+		out.close();
+		isOpen = false;
+	}
+}
+
+// Cast to an istream by casting the bound ipipe to an istream:
+iopipe::operator std::istream& () {
+	return in;
+}
+
+// Cast to an ostream by casting the bound opipe to an ostream:
+iopipe::operator std::ostream& () {
+	return out;
+}
+
+// Return the input file descriptor:
+const int iopipe::in_file() {
+	return in.file();
+}
+
+// Return the output file descriptor:
+const int iopipe::out_file() {
+	return out.file();
+}
+
+// Return the open state of the iopipe:
+const bool iopipe::is_open() {
+	return isOpen;
+}
+
+// Bind standard input to the input of this iopipe by invoking the ipipe's own 'bind' method:
+void iopipe::bind_in() {
+	in.bind();
+}
+
+// Bind standard output to the output of this iopipe by invoking the opipe's own 'bind' method:
+void iopipe::bind_out() {
+	out.bind();
+}
+
+/***********************************************************************************************************/
---
+++ Arbiter/iopipe/ipipe.cpp
@@ -0,0 +1,131 @@
+/* -------------------------------------------
+ * ipipe - ipipe.cpp
+ * -------------------------------------------
+ * Implements the 'ipipe' class, which manages unix pipe input via a stream interface.
+ * -------------------------------------------
+ * Author:  James Payor (github.com/amoebius)
+ * -------------------------------------------
+ */
+
+/* NOTE:  (IMPORTANT)
+ * This module needs a hacky method to convert a pipe file descriptor into istreams
+ * ostreams, or replicate that functionality.  The author envisages two methods:
+ *  1) Use stringstreams to replicate high-level stream io, and write/read from the pipe
+ *     to fill/empty the stringstreams.
+ *  2) Use non-standard classes to build a stream around a POSIX file descriptor.
+ *
+ * THIS implementation uses OPTION 2.  There is a chance that it will not complie given
+ * that "__gnu_cxx::stdio_filebur<char>" is used.  If this is the case, the functionality
+ * can be replicated with stringstreams, but for now this has not been implemented, as
+ * wrapping the file descriptors directly simplifies matters considerably.
+ *
+ */
+
+
+#include "ipipe.hpp"
+
+
+/***********************************************************************************************************/
+/*  "ipipe_ref" Implementation:                                                                            */
+/***********************************************************************************************************/
+
+// Construct from a file descriptor through a simple initialiser list:
+ipipe::ipipe_ref::ipipe_ref(int fd) : instances(1), fd(fd), buffer(fd, std::ios::in), stream(&buffer) {}
+
+// On destruction, close the buffer and the file descriptor:
+ipipe::ipipe_ref::~ipipe_ref() {
+	buffer.close();
+	close(fd);
+}
+
+// Increment the reference count:
+void ipipe::ipipe_ref::inc() {
+	++instances;
+}
+
+// Decrement the reference count:
+void ipipe::ipipe_ref::dec() {
+	--instances;
+	if(!instances) delete this; // Delete this when orphaned!
+}
+
+// To cast to an istream, simply return the stream:
+ipipe::ipipe_ref::operator std::istream& () {
+	return stream;
+}
+
+// Returns the file descriptor bound to the object:
+const int ipipe::ipipe_ref::file() {
+	return fd;
+}
+
+/***********************************************************************************************************/
+
+
+
+
+/***********************************************************************************************************/
+/*  "ipipe" Implementation:                                                                                */
+/***********************************************************************************************************/
+
+// Constructs a 'closed' ipipe - sets isOpen to false:
+ipipe::ipipe() : pipe(NULL), isOpen(false) {}
+
+// Constructs an ipipe from the given file descriptor - creates a new ipipe_ref, and marks the ipipe open:
+ipipe::ipipe(int fd) : pipe(new ipipe_ref(fd)), isOpen(true) {}
+
+// Copy constructor - copies the state of the other object, and increments the ipipe_ref's reference count if necessary:
+ipipe::ipipe(const ipipe& other) : pipe(other.pipe), isOpen(other.isOpen) {
+	if(isOpen) pipe->inc();
+}
+
+// Assignment operator - close any current file, then update as with the copy constructor:
+ipipe& ipipe::operator = (const ipipe& other) {
+	close();
+	pipe = other.pipe;
+	isOpen = other.isOpen;
+	if(isOpen) pipe->inc();
+}
+
+// Destructor - close any current file:
+ipipe::~ipipe() {
+	close();
+}
+
+// Open the given file descriptor, closing an existing file first, then imitates the ipipe(int fd) constructor:
+void ipipe::open(int fd) {
+	close();
+	pipe = new ipipe_ref(fd);
+	isOpen = true;
+}
+
+// Close the ipipe - decrement the ipipe_ref's reference count if open, and update open state:
+void ipipe::close() {
+	if(isOpen) {
+		pipe->dec();
+		pipe = NULL;
+		isOpen = false;
+	}
+}
+
+// Cast to an istream - return the ipipe_ref casted to an istream:
+ipipe::operator std::istream& () {
+	return *pipe;
+}
+
+// Returns the ipipe_ref's file descriptor:
+const int ipipe::file() {
+	return pipe->file();
+}
+
+// Binds standard input to this file descriptor - straightforward invokation of the 'dup2' api method:
+void ipipe::bind() {
+	dup2(pipe->file(), STDIN_FILENO);
+}
+
+// Returns the open status of the ipipe:
+const bool ipipe::is_open() {
+	return isOpen;
+}
+
+/***********************************************************************************************************/
