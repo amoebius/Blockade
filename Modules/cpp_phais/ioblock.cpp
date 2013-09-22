675e3810fc221a3d3450745b12c0d154c6b0133d
James Payor <j.payor@hotmail.com>
2013-09-22 18:13:16 +1000

    stderr blocking enabled.

--- Modules/cpp_phais/ioblock.cpp
+++ Modules/cpp_phais/ioblock.cpp
@@ -24,20 +24,23 @@ typedef __gnu_cxx::stdio_filebuf<char> fdbuffer;
 
 namespace ioblock {
 
-	int infd = dup(STDIN_FILENO), outfd = dup(STDOUT_FILENO), devnull = open("/dev/null", O_RDWR);
-	fdbuffer *out, *in, *nullout, *nullin;
+	int infd = dup(STDIN_FILENO), outfd = dup(STDOUT_FILENO), errfd = dup(STDERR_FILENO), devnull = open("/dev/null", O_RDWR);
+	fdbuffer *out, *in, *err, *nullout, *nullin;
 
 	void init() {
 
 		infd = dup(STDIN_FILENO);
 		outfd = dup(STDOUT_FILENO);
+		errfd = dup(STDERR_FILENO);
 		devnull = open("/dev/null", O_RDWR);
 
 		out = new fdbuffer(outfd, std::ios::out);
 		in = new fdbuffer(infd, std::ios::in);
+		err = new fdbuffer(errfd, std::ios::out);
 
 		dup2(devnull, STDIN_FILENO);
 		dup2(devnull, STDOUT_FILENO);
+		dup2(devnull, STDERR_FILENO);
 
 		nullout = new fdbuffer(STDOUT_FILENO, std::ios::out);
 		nullin = new fdbuffer(STDIN_FILENO, std::ios::in);
@@ -48,11 +51,13 @@ namespace ioblock {
 	void block() {
 		std::cout.rdbuf(nullout);
 		std::cin.rdbuf(nullin);
+		std::cerr.rdbuf(nullout);
 	}
 
 	void unblock() {
 		std::cout.rdbuf(out);
 		std::cin.rdbuf(in);
+		std::cerr.rdbuf(err);
 	}
 
 }; // namespace ioblock
