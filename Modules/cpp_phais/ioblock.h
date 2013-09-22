4fda35221d5e8f44c1bf3bd011072981d090c829
James Payor <j.payor@hotmail.com>
2013-09-22 17:21:38 +1000

    IO blocking routines rewritten - hopefully adequate now.

--- Modules/cpp_phais/ioblock.h
+++ Modules/cpp_phais/ioblock.h
@@ -1,10 +1,10 @@
 /* -------------------------------------------
  * Blockade Modules - ioblock.h
  * -------------------------------------------
- * Implements useful methods for temporarily
- * blocking reading and writing to stdin and
- * stdout, to prevent modules from intefering
- * with arbiter-module communication.
+ * Implements a very hacky, but very useful
+ * method for blocking reading and writing to
+ * stdin and stdout , to prevent modules from
+ * intefering with the arbiter-module stream.
  * -------------------------------------------
  * Author:  James Payor (github.com/amoebius)
  * -------------------------------------------
@@ -13,29 +13,12 @@
 #ifndef __IOBLOCK_H
 #define __IOBLOCK_H
 
-#include <unistd.h>
-#include <fcntl.h>
+namespace ioblock {
 
-static int infd = -1, outfd = -1, devnull = -1;
+	void init();
+	void block();
+	void unblock();
 
-void ioblock() {
-
-	if(infd < 0 || outfd < 0 || devnull < 0) {
-		infd = dup(STDIN_FILENO);
-		outfd = dup(STDOUT_FILENO);
-		devnull = open("/dev/null", O_RDWR);
-	}
-
-	dup2(devnull, STDIN_FILENO);
-	dup2(devnull, STDOUT_FILENO);
 }
-
-void iounblock() {
-	if(infd > 0 && outfd > 0) {
-		dup2(infd, STDIN_FILENO);
-		dup2(outfd, STDOUT_FILENO);
-	}
-}
-
 
 #endif //__IOBLOCK_H
