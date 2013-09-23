bc3dd7d3bb96e202ffc199e12f8aeb27f5a0a3e3
James Payor <j.payor@hotmail.com>
2013-09-23 20:20:23 +1000

    Bots compiled and set up for testing.

--- Bots/py_matts1/gaff.py
+++ Bots/py_matts1/gaff.py
@@ -5,9 +5,9 @@ from collections import deque
 from random import randint
 
 d = ((0,1), (0,-1), (1,0), (-1,0))
-moves = dict(zip('UDLR', d))
+moves = dict(zip('DURL', d))
 
-dickishness = 2
+dickishness = 1
 
 c = lambda b: map(list,b)
 def b(board, (x,y)):
@@ -18,26 +18,28 @@ def b(board, (x,y)):
 def dist(board, (x,y), goal):
 	q = deque()
 	q.append((0,x,y))
+	if board[y][x]: return -1
 	board[y][x] = True
 	s = len(board)
 	while q:
-		d,x,y = q.popleft()
-		if y == goal: return d
+		dist,x,y = q.popleft()
+		if y == goal: return dist
 		for dx,dy in d:
 			if 0 <= x+dx < s and 0 <= y+dy < s:
 				if not board[y+dy][x+dx]:
-					q.append((d+1,x+dx,y+dy))
+					board[y+dy][x+dx] = True
+					q.append((dist+1,x+dx,y+dy))
 	return -1
 
-def randmin(iterable):
+def randmin(iterable, key = lambda x: x[0] if type(x) is tuple else x):
 	iterator = iter(iterable)
 	low = next(iterator)
 	count = 1
 	for elem in iterator:
-		if elem < low:
+		if key(elem) < key(low):
 			low = elem
 			count = 1
-		elif elem == low:
+		elif key(elem) == key(low):
 			if randint(0,count) == 0: low = elem
 			count += 1
 	return low
@@ -47,7 +49,11 @@ def move(board, (x,y), (ox,oy)):
 	
 	s = len(board)
 	dmove, move = randmin((d,m) for d,m in ((dist(c(board), (x+dx,y+dy), s-1), move) for move, (dx,dy) in moves.items() if 0 <= x+dx < s and 0 <= y+dy < s) if d >= 0)
-	dmove -= dist(c(board), (ox,oy), 0)
-	dblock, block = randmin((theirdist - mydist, block) for mydist, theirdist, block in ((dist(b(board,(bx,by)), (x,y), s-1), dist(b(board,(bx,by)), (ox,oy), 0), (bx,by)) for by in range(s) for bx in range(x) if not board[by][bx]) if mydist >= 0 and theirdist >= 0)
+	
+	try:
+		dblock, block = randmin((mydist - theirdist, block) for mydist, theirdist, block in ((dist(b(board,(bx,by)), (x,y), s-1), dist(b(board,(bx,by)), (ox,oy), 0), (bx,by)) for by in range(s) for bx in range(s) if not board[by][bx]) if mydist >= 0 and theirdist >= 0)
+		dblock = dblock - (dmove+1 - dist(c(board), (ox,oy), 0))
+	except StopIteration:
+		return move
 
-	return move if dmove > dickishness * dblock else block
+	return move if 0 <= dickishness * dblock else block
