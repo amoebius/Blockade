0041676c7ca044f5c74607f0619fd30989313f6e
James Payor <j.payor@hotmail.com>
2013-10-16 17:02:37 +1100

    Include statement fixed......

--- Modules/cpp_phais/blockade.cpp
+++ Modules/cpp_phais/blockade.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 #include "blockade.h"
-#include "../ioblock/ioblock.h"
+#include "../ioblock/ioblock.hpp"
 
 // Command strings:
 static const string str_move("move"), str_block("block"), str_turn("turn"), str_end("end"), str_nothing("nothing");
