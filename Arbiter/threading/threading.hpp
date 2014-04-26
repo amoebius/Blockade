d9abfbefbd737c4ec51f3fc7aedb1f26e4a4d2be
James Payor <j.payor@hotmail.com>
2014-04-27 02:33:11 +1000

    Added cancel functionality to the higher level threading library.

--- Arbiter/threading/threading.hpp
+++ Arbiter/threading/threading.hpp
@@ -42,138 +42,6 @@ public:
 
 namespace Threading {
 
-	// Declare the existence of the "Threaded" class:
-	template<class FunctorT> class Threaded;
-
-
-	// Define the worker class, "ThreadRunner", responsible for coordinating the results of threads:
-	template <class FunctorT>
-	class ThreadRunner : virtual Thread, FunctorT {
-		friend class Thread;
-
-	private:
-		// Record the result type:
-		typedef typeof((*((FunctorT*)0))()) ResultT;
-
-		// Whether the thread is currently running or not:
-		bool running;
-		// The result of the threaded functor:
-		ResultT result;
-
-		friend class Threaded<FunctorT>;
-		// Whether the result is still required or not:
-		bool required;
-
-	public:
-		// Initialise the thread:
-		ThreadRunner(const FunctorT &functor, int priority = HT::Thread::Priority::Normal) : FunctorT(functor), required(true) {
-			running = true;
-			threadID = HT::Thread::Create(run, this);
-			setPriority(priority);
-		}
-
-		// A simple helper definition:
-		inline bool finished() {
-			return !running;
-		}
-
-		// To return the result, join with the thread, then grab the result:
-		ResultT operator()() {
-			HT::Thread::Join(threadID);
-			return result;
-		}
-
-	private:
-		// Given a void* to a ThreadRunner object, runs a thread:
-		static void *run(void *object) {
-			// Convert the void*:
-			ThreadRunner<FunctorT> *obj = static_cast<ThreadRunner<FunctorT>*>(object);
-
-			// Run the thread:
-			obj->running = true;
-			obj->result = obj->FunctorT::operator()();
-			obj->running = false;
-
-			// Delete the object if the result isn't required:
-			if(!obj->required) delete obj;
-
-			return NULL;
-		}
-	};
-
-	// Declares the threaded class, which stores a reference to the working ThreadRunner, so that the thread can continue should this
-	// object be destroyed:
-	template<class FunctorT>
-	class Threaded {
-		friend class ThreadRunner<FunctorT>;
-
-	private:
-		// The actual ThreadRunner object handling the thread:
-		ThreadRunner<FunctorT> *thread;
-
-	public:
-		// Initialise the thread runner on instantiation:
-		Threaded(FunctorT functor, int priority = HT::Thread::Priority::Normal) : thread(new ThreadRunner<FunctorT>(functor,priority)) {}
-
-		// Get the functor's return type:
-		typedef typeof((*((FunctorT*)0))()) ResultT;
-
-		// Methods for getting the result, as if this were a functor:
-		inline ResultT operator()() {
-			return thread->operator()();
-		}
-		inline ResultT join() {
-			return thread->operator()();
-		}
-
-		// On destruction:
-		~Threaded() {
-			// If the thread is finished, we can destroy it, otherwise we mark it as not required:
-			if(thread->finished()) {
-				delete thread;
-			} else {
-				thread->required = false;
-			}
-		}
-
-		// Helper function:
-		inline bool finished() {
-			return thread->finished();
-		}
-	};
-
-	// A simple functor that wraps void functions to be able to be run as threads:
-	class FunctionWrapper {
-	private:
-		typedef void FuncT(void);
-		// Pointer to the wrapped function:
-		FuncT *func;
-	public:
-		FunctionWrapper(FuncT *func) : func(func) {}
-		// Calling the functor simply calls the wrapped function:
-		inline threadvoid operator()() {
-			(*func)();
-			return 0;
-		}
-	};
-
-
-	// A helper function that simply creates a threaded version of the given functor:
-	template<typename FunctorT>
-	static inline Threaded<FunctorT> Create(FunctorT functor, int priority = HT::Thread::Priority::Normal) {
-		return Threaded<FunctorT>(functor, priority);
-	}
-
-	// A specialisation of the helper function that builds a thread from a void function:
-	static inline Threaded<FunctionWrapper> Create(void (*func)(void), int priority = HT::Thread::Priority::Normal) {
-		return Threaded<FunctionWrapper>(FunctionWrapper(func), priority);
-	}
-
-	// Helpful type definition:
-	typedef Threaded<FunctionWrapper> ThreadedFunc;
-
-
-
 	// A wrapper around Hawkthreads' mutex interface:
 	class Mutex {
 	private:
@@ -219,6 +87,154 @@ namespace Threading {
 			return HT::Cond::Broadcast(cond);
 		}
 	};
+
+
+
+
+	// Declare the existence of the "Threaded" class:
+	template<class FunctorT> class Threaded;
+
+
+	// Define the worker class, "ThreadRunner", responsible for coordinating the results of threads:
+	template <class FunctorT>
+	class ThreadRunner : virtual Thread, FunctorT {
+		friend class Thread;
+
+	private:
+		// Record the result type:
+		typedef typeof((*((FunctorT*)0))()) ResultT;
+
+		// Mutex to manage state of the thread:
+		Mutex lock;
+
+		// Whether the thread is currently running or not:
+		bool running;
+		// The result of the threaded functor:
+		ResultT result;
+
+		friend class Threaded<FunctorT>;
+		// Whether the result is still required or not:
+		bool required;
+
+	public:
+		// Initialise the thread:
+		ThreadRunner(const FunctorT &functor, int priority = HT::Thread::Priority::Normal) : FunctorT(functor), required(true) {
+			running = true;
+			threadID = HT::Thread::Create(run, this);
+			setPriority(priority);
+		}
+
+		// To return the result, join with the thread, then grab the result:
+		ResultT operator()() {
+			HT::Thread::Join(threadID);
+			return result;
+		}
+
+	private:
+		// Given a void* to a ThreadRunner object, runs a thread:
+		static void *run(void *object) {
+			// Convert the void*:
+			ThreadRunner<FunctorT> *obj = static_cast<ThreadRunner<FunctorT>*>(object);
+
+			// Run the thread:
+			obj->result = obj->FunctorT::operator()();
+			obj->lock.Lock();
+			obj->running = false;
+			bool required = obj->required;
+			obj->lock.Unlock();
+
+			// Delete the object if the result isn't required:
+			if(!required) delete obj;
+
+			return NULL;
+		}
+	};
+
+	// Declares the threaded class, which stores a reference to the working ThreadRunner, so that the thread can continue should this
+	// object be destroyed:
+	template<class FunctorT>
+	class Threaded {
+		friend class ThreadRunner<FunctorT>;
+
+	private:
+		// The actual ThreadRunner object handling the thread:
+		ThreadRunner<FunctorT> *thread;
+
+	public:
+		// Initialise the thread runner on instantiation:
+		Threaded(FunctorT functor, int priority = HT::Thread::Priority::Normal) : thread(new ThreadRunner<FunctorT>(functor,priority)) {}
+
+		// Get the functor's return type:
+		typedef typeof((*((FunctorT*)0))()) ResultT;
+
+		// Methods for getting the result, as if this were a functor:
+		inline ResultT operator()() {
+			return thread->operator()();
+		}
+		inline ResultT join() {
+			return thread->operator()();
+		}
+
+		// On destruction:
+		~Threaded() {
+			// If the thread is finished, we can destroy it, otherwise we mark it as not required:
+			thread->lock.Lock();
+			if(thread->running) {
+				thread->required = false;
+				thread->lock.Unlock();
+			} else {
+				thread->lock.Unlock();
+				delete thread;
+			}
+		}
+
+		// Helper function:
+		inline bool finished() {
+			thread->lock.Lock();
+			bool finished = !thread->running;
+			thread->lock.Unlock();
+			return finished;
+		}
+
+		// Cancels the current thread:
+		bool cancel() {
+			thread->lock.Lock();
+			if(!thread->running) return true;
+			thread->required = false;
+			thread->lock.Unlock();
+			return HT::Thread::Cancel(threadID);
+		}
+	};
+
+	// A simple functor that wraps void functions to be able to be run as threads:
+	class FunctionWrapper {
+	private:
+		typedef void FuncT(void);
+		// Pointer to the wrapped function:
+		FuncT *func;
+	public:
+		FunctionWrapper(FuncT *func) : func(func) {}
+		// Calling the functor simply calls the wrapped function:
+		inline threadvoid operator()() {
+			(*func)();
+			return 0;
+		}
+	};
+
+
+	// A helper function that simply creates a threaded version of the given functor:
+	template<typename FunctorT>
+	static inline Threaded<FunctorT> Create(FunctorT functor, int priority = HT::Thread::Priority::Normal) {
+		return Threaded<FunctorT>(functor, priority);
+	}
+
+	// A specialisation of the helper function that builds a thread from a void function:
+	static inline Threaded<FunctionWrapper> Create(void (*func)(void), int priority = HT::Thread::Priority::Normal) {
+		return Threaded<FunctionWrapper>(FunctionWrapper(func), priority);
+	}
+
+	// Helpful type definition:
+	typedef Threaded<FunctionWrapper> ThreadedFunc;
 
 }
 
